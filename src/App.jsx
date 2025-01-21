42c216b48b73be982a78aa5fe171156cd7699119
euphaustus <43821808+euphaustus@users.noreply.github.com>
2025-01-20 20:09:44 -0700

    fix css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
-import './app.css';
+import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 
