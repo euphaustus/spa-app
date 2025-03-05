94ab634af41dd2bde45b245ee9609269d52ef96b
euphaustus <43821808+euphaustus@users.noreply.github.com>
2025-03-05 16:17:56 -0700

    hardcode SLC for now

--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -19,7 +19,7 @@ function WeatherComponent() {
 
         const locationResponse = await fetch('https://ip-api.com/json'); // Direct fetch to ip-api.com
         if (!locationResponse.ok) {
-          throw new Error(`IP API Error: ${locationResponse.status} - ${locationResponse.statusText}`);
+          setCity("Salt Lake City")
         }
 
         const locationData = await locationResponse.json();
