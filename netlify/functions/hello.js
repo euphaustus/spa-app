abc7ac3b72d8e6c6ae9367f58a8cc1b5e1c8c1e8
euphaustus <43821808+euphaustus@users.noreply.github.com>
2025-04-08 14:14:47 -0600

    testing

---
+++ netlify/functions/hello.js
@@ -0,0 +1,10 @@
+// netlify/functions/hello.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: "Hello from Netlify Function (CommonJS)!" }),
+  };
+};
